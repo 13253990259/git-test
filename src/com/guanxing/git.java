6ffc7830afbd23a4ec7be8c0c6f1f99890ceeef2
liuguanxing <173613742@qq.com>
2019-04-16 19:57:51 +0800

    将git提交到本地仓库2

--- src/com/guanxing/git.java
+++ src/com/guanxing/git.java
@@ -4,4 +4,8 @@ public class git {
     public static void main(String[] args) {
         System.out.println("你好!");
     }
+
+    public void test(){
+        System.out.println("你是最棒的!");
+    }
 }
